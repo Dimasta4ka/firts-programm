5d59e22ba45d416720ad6b8c0cb279d3cc05707e
Dimasta4ka <kiselevich.dima@yandex.ru>
2022-11-30 09:02:58 +0300

    converting fahrenheit to Celsius and vice versa

--- variables_and_arithmetic_expressions.c
+++ variables_and_arithmetic_expressions.c
@@ -9,11 +9,24 @@ main(){
     step = 20;
 
     fahr = lower;
+    printf("Table of fahrenheit temperature in Celsius\n");
     while (fahr <= upper)
     {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%3.0f\t %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
-    
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    celsius = lower;
+    printf("Temperature table celsius to fahrenheit\n");
+    while (celsius <= upper)
+    {
+        fahr = (9.0 / 5.0) * celsius + 32.0;
+        printf("%3.0f\t %6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
 }
